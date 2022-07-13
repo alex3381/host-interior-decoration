7fbf2e6c3c38b6e6d79f4d676d2381a4f118ef86
fatogun <diodio2286@gmail.com>
2022-07-13 18:12:24 +0100

    Built web

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -50,8 +50,8 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "2b193cc532ec9fcd39761d3eee41bf29",
-"/": "2b193cc532ec9fcd39761d3eee41bf29",
+"index.html": "fe604d3dfc58a9e1e87e9596460c4615",
+"/": "fe604d3dfc58a9e1e87e9596460c4615",
 "main.dart.js": "a8868f05172880c8bd57a5233abb65f6",
 "manifest.json": "2b1f7f6a1fa3051d33ec860abe7c5721",
 "version.json": "d687983d2c839ad1dc7ef45f5c5fa0df"
